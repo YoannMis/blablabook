b5abed9bc0c6bdd6047ed060412d9d79b9645172
ikomatsai <hache@DESKTOP-SPVDSAH.localdomain>
2026-03-16 14:22:06 +0100

    deplacement du schema zod dans un dossier avec modification du schema

--- front/src/components/Register.tsx
+++ front/src/components/Register.tsx
@@ -2,26 +2,9 @@ import z from 'zod';
 // import { zodResolver } from '@hookform/resolvers/zod';
 // import { useForm } from 'react-hook-form';
 import { Box, Field, FieldErrorText, Input, Button, Heading } from '@chakra-ui/react';
+import {RegisterSchema} from '../schema/register.schema.ts';
 
-// //Création du Schema Zod
-// const RegisterSchema = z.object({
-//   username: z.string().min(1, 'Username is required'),
-//   email: z.string().email('Email must be a valid email address').min(1, 'Email is required'),
-//   password: z
-//     .string()
-//     .min(8, 'Password must be at least 8 characters long')
-//     .max(14, 'Password must be at most 14 characters long')
-//     .regex(
-//       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()]).{8,14}$/,
-//       'Password must contain at least 1 lowercase, 1 uppercase, 1 number, and 1 special character (!@#$%^&*)'
-//     ),
-//   confirmPassword: z
-//     .string()
-//     .min(1, 'Confirm Password is required')
-//     .refine((value, { parent }) => value === parent.password, {
-//       message: 'Passwords do not match',
-//     }),
-// });
+
 
 // //Typage Typescript
 // type RegisterFormValues = z.infer<typeof RegisterSchema>;
