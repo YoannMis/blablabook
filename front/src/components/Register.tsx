1c3051c43c2a414a9f83df6435ab949f45867bb4
ikomatsai <hache@DESKTOP-SPVDSAH.localdomain>
2026-03-17 14:31:52 +0100

    fix: make the submit button accessible only when there are no errors and all inputs are filled, update the Zod validation rules for the confirmed password

--- front/src/components/Register.tsx
+++ front/src/components/Register.tsx
@@ -1,5 +1,5 @@
 import z, { string } from 'zod';
-import { Box, Field, Input, Button, Heading, FieldErrorIcon, Flex } from '@chakra-ui/react';
+import { Box, Field, Input, Button, Heading, FieldErrorIcon, Flex, VStack } from '@chakra-ui/react';
 import { PasswordInput } from '../components/ui/password-input.tsx';
 import RegisterSchema from '../schema/register.schema.ts';
 import axios, { AxiosError } from 'axios';
@@ -10,15 +10,15 @@ import { RiArrowRightLine } from 'react-icons/ri';
 type RegisterFormValues = z.infer<typeof RegisterSchema>;
 
 //Formulaire
-const Register = (values: RegisterFormValues) => {
-  const [userInfos, setUserInfos] = useState({
+const Register = () => {
+  const [userInfos, setUserInfos] = useState<RegisterFormValues>({
     username: '',
     email: '',
     password: '',
     confirmPassword: '',
   });
 
-  const [errors, setErrors] = useState({
+  const [errors, setErrors] = useState<RegisterFormValues>({
     username: '',
     email: '',
     password: '',
@@ -27,12 +27,15 @@ const Register = (values: RegisterFormValues) => {
 
   const [isSubmitting, setIsSubmitting] = useState(false);
 
+  const isFormInvalid =
+    Object.values(errors).some((error) => error.length > 0) ||
+    Object.values(userInfos).some((value) => !value);
+
   // fonction de soumission du formulaire
   // en attente du back pour les responses de axios
   const handleSubmit = async (event: React.SubmitEvent) => {
     event.preventDefault();
     setIsSubmitting(true);
-
     try {
       const response = await axios.post('/api/register', userInfos);
     } catch (error) {
@@ -81,67 +84,75 @@ const Register = (values: RegisterFormValues) => {
   return (
     <Flex justify="center">
       <Box as="form" onSubmit={handleSubmit}>
-        <Heading size="3xl">Register</Heading>
-        <Field.Root invalid={!!errors.username}>
-          <Field.Label>Username</Field.Label>
-          <Input
-            name="username"
-            placeholder="Username"
-            value={userInfos.username}
-            onChange={handleChange}
-          />
-          <Field.ErrorText>
-            <FieldErrorIcon />
-            {errors.username}
-          </Field.ErrorText>
-        </Field.Root>
+        <VStack gap={4}>
+          <Heading size="3xl">Register</Heading>
+          <Field.Root invalid={!!errors.username}>
+            <Field.Label>Username</Field.Label>
+            <Input
+              name="username"
+              placeholder="Username"
+              value={userInfos.username}
+              onChange={handleChange}
+            />
+            <Field.ErrorText>
+              <FieldErrorIcon />
+              {errors.username}
+            </Field.ErrorText>
+          </Field.Root>
 
-        <Field.Root invalid={!!errors.email}>
-          <Field.Label>Email</Field.Label>
-          <Input name="email" placeholder="Email" value={userInfos.email} onChange={handleChange} />
-          <Field.ErrorText>
-            <FieldErrorIcon />
-            {errors.email}
-          </Field.ErrorText>
-        </Field.Root>
+          <Field.Root invalid={!!errors.email}>
+            <Field.Label>Email</Field.Label>
+            <Input
+              name="email"
+              placeholder="Email"
+              value={userInfos.email}
+              onChange={handleChange}
+            />
+            <Field.ErrorText>
+              <FieldErrorIcon />
+              {errors.email}
+            </Field.ErrorText>
+          </Field.Root>
 
-        <Field.Root invalid={!!errors.password}>
-          <Field.Label>Password</Field.Label>
-          <PasswordInput
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={userInfos.password}
-            onChange={handleChange}
-          />
-          <Field.ErrorText>
-            <FieldErrorIcon /> {errors.password}
-          </Field.ErrorText>
-        </Field.Root>
+          <Field.Root invalid={!!errors.password}>
+            <Field.Label>Password</Field.Label>
+            <PasswordInput
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={userInfos.password}
+              onChange={handleChange}
+            />
+            <Field.ErrorText>
+              <FieldErrorIcon /> {errors.password}
+            </Field.ErrorText>
+          </Field.Root>
 
-        <Field.Root invalid={!!errors.confirmPassword}>
-          <Field.Label>Confirm your Password</Field.Label>
-          <PasswordInput
-            name="confirmPassword"
-            type="password"
-            placeholder="Confirm your Password"
-            value={userInfos.confirmPassword}
-            onChange={handleChange}
-          />
-          <Field.ErrorText>
-            <FieldErrorIcon />
-            {errors.confirmPassword}
-          </Field.ErrorText>
-        </Field.Root>
-        <Button
-          loading={isSubmitting}
-          onClick={() => setIsSubmitting(!isSubmitting)}
-          loadingText="Submitting ..."
-          type="submit"
-        >
-          Submit
-          <RiArrowRightLine />
-        </Button>
+          <Field.Root invalid={!!errors.confirmPassword}>
+            <Field.Label>Confirm your Password</Field.Label>
+            <PasswordInput
+              name="confirmPassword"
+              type="password"
+              placeholder="Confirm your Password"
+              value={userInfos.confirmPassword}
+              onChange={handleChange}
+            />
+            <Field.ErrorText>
+              <FieldErrorIcon />
+              {errors.confirmPassword}
+            </Field.ErrorText>
+          </Field.Root>
+          <Button
+            disabled={isFormInvalid}
+            loading={isSubmitting}
+            onClick={() => setIsSubmitting(!isSubmitting)}
+            loadingText="Submitting ..."
+            type="submit"
+          >
+            Submit
+            <RiArrowRightLine />
+          </Button>
+        </VStack>
       </Box>
     </Flex>
   );
