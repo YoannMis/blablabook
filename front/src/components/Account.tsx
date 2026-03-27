99ce84f644111e6cd0c8ff736b9ade4877d3c977
ikomatsai <hachet_stephane@hotmail.com>
2026-03-27 13:07:20 +0100

    fix: deleted bad import

--- front/src/components/Account.tsx
+++ front/src/components/Account.tsx
@@ -1,13 +1,7 @@
-import { useCurrentUser } from '../context/UserContext';
-import { useTranslation } from 'react-i18next';
-import { Box, Heading, Text, VStack } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import ChangeLanguages from './ChangeLanguages';
 
 const AccountPage = () => {
-  const { user } = useCurrentUser();
-
-  const { t } = useTranslation();
-
   return (
     <Box>
       <ChangeLanguages />
