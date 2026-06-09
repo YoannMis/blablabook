45af9590d616c03af0d8634baae872f1d12b9b0b
YoannMis <ymisericordia@gmx.com>
2026-06-09 12:16:16 +0200

    fix(homePage): removed unused imports

--- front/src/components/HomePage.tsx
+++ front/src/components/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
